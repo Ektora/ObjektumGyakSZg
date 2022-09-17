464b413f48121683c45a14dba64aff6f9c3543a8
Ektora <zsoltlucski@gmail.com>
2022-09-17 16:00:35 +0200

    Add project files.

---
+++ ObjektumGyakSZg/Macska.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjektumGyakSZg
+{
+    internal class Macska
+    {
+        /*
+
+
+        • Az osztálynak legyen egy void futkos metódusa, ami nem vár paramétert. A macska
+súlya csökkenjen 0.1-el, és ha nem volt éhes,
+        */
+        public String nev;
+        public double suly;
+        public bool ehese;
+
+        public Macska(string nev, double suly, bool ehese)
+        {
+            this.nev = nev;
+            this.suly = suly;
+            this.ehese = ehese;
+        }
+
+        public Macska(string name, double suly) : this(name, suly, true)
+        {
+
+        }
+
+        public void Futkos()
+        {
+            this.suly -= 0.10;
+            if (!this.ehese)
+                this.ehese = true;
+        }
+
+        public bool Eszik(double etelSuly)
+        {
+            if (!ehese)
+            {
+                return false;
+            }
+            this.suly += etelSuly;
+            this.ehese = !this.ehese;
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"A macska súlya {Math.Round(this.suly,2)} kg és{(this.ehese ? "" : " nem")} éhes.";
+        }
+
+    }
+}
---
+++ ObjektumGyakSZg/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using ObjektumGyakSZg;
+
+public class ObjektumGyak{
+
+    public static void Main(string[] args)
+    {
+        Macska m1 = new Macska("Cirmi", 2.3, false);
+        Macska m2 = new Macska("Kormos", 1.4);
+
+        m1.Eszik(0.2);
+
+        Console.WriteLine(m1);
+        m1.Futkos();
+        Console.WriteLine(m1);
+        m2.Futkos();
+        m2.Futkos();
+        m2.Eszik(0.3);
+        m2.Futkos();
+        Console.WriteLine(m2);
+    }
+
+}
