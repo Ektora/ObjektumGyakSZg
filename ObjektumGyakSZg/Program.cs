d6923dd18870229aeeda7d2644e92c130309be74
Ektora <zsoltlucski@gmail.com>
2022-10-16 11:19:28 +0200

    Argumentumok gyakorlása 1.

--- ObjektumGyakSZg/Program.cs
+++ ObjektumGyakSZg/Program.cs
@@ -1,15 +1,31 @@
 ﻿
 using ObjektumGyakSZg;
+using System.Numerics;
 
 public class ObjektumGyak{
 
     public static void Main(string[] args)
     {
+        if(args.Length < 2)
+        {
+            Console.WriteLine("Nincs elég bemenet megadva");
+        }
+        else
+        {
+            int min = int.MaxValue;
+            for(int i = 0; i< args.Length; i++)
+            {
+                if(min > int.Parse(args[i]))
+                    min = int.Parse(args[i]);
+            }
+            Console.WriteLine($"A legkisebb szám: {min}");
+
+        }
+
+        /*
         Macska m1 = new Macska("Cirmi", 2.3, false);
         Macska m2 = new Macska("Kormos", 1.4);
-
         m1.Eszik(0.2);
-
         Console.WriteLine(m1);
         m1.Futkos();
         Console.WriteLine(m1);
@@ -18,6 +34,17 @@ public class ObjektumGyak{
         m2.Eszik(0.3);
         m2.Futkos();
         Console.WriteLine(m2);
+        
+        Szamitogep szg1 = new Szamitogep();
+        Szamitogep szg2 = new Szamitogep(2000, false);
+        szg1.Kapcsol();
+        Console.WriteLine($"A számítógépre a másolás sikeres: {(szg1.Masol(400) ? "igen" : "nem")}");
+        Console.WriteLine($"A számítógépre a másolás sikeres: {(szg1.Masol(800) ? "igen" : "nem")}");
+        Console.WriteLine($"A számítógépre a másolás sikeres: {(szg2.Masol(1) ? "igen" : "nem")}");
+        Console.WriteLine(szg1);
+        Console.WriteLine(szg2);
+
+        */
     }
 
 }
---
+++ ObjektumGyakSZg/Szamitogep.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjektumGyakSZg
+{
+    internal class Szamitogep
+    {
+        public double szabadMB;
+        public bool beKapcsolt;
+
+        public Szamitogep(double szabadMB, bool beKapcsolt)
+        {
+            this.szabadMB = szabadMB;
+            this.beKapcsolt = beKapcsolt;
+        }
+
+        public Szamitogep() : this(1024,false)
+        {
+
+        }
+
+        public void Kapcsol()
+        {
+            beKapcsolt = !beKapcsolt;
+        }
+
+        public bool Masol(double memoria)
+        {
+            if (beKapcsolt)
+            {
+                if (szabadMB >= memoria)
+                {
+                    this.szabadMB -= memoria;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"A számítógép {(beKapcsolt ? "bekapcsolt" : "kikapcsolt")} állapotú {szabadMB} MB memóriával";
+        }
+    }
+}
