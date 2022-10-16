2f454cb80246e378f5a208377f05d0c14aaaf252
Ektora <zsoltlucski@gmail.com>
2022-10-16 16:52:45 +0200

    Argumentumos feladatok 3.

--- ObjektumGyakSZg/Program.cs
+++ ObjektumGyakSZg/Program.cs
@@ -6,7 +6,35 @@ public class ObjektumGyak{
 
     public static void Main(string[] args)
     {
-        if(args.Length < 2)
+        //vegrehajtArgFeladatok1(args);
+        vegrehajtArgFeladatok2(args);
+        
+    }
+    static void vegrehajtArgFeladatok2(string[] args)
+    {
+        int szam, osszesPozitiv=0, osszesNegativ = 0, osszesNulla = 0;
+        if (args.Length == 0)
+            Console.WriteLine("Nincs bemeneti argumentum");
+        else
+        {
+            for(int i = 0; i < args.Length; i++)
+            {
+                szam = int.Parse(args[i]);
+                if (szam > 0)
+                    osszesPozitiv++;
+                else if (szam < 0)
+                    osszesNegativ++;
+                else
+                    osszesNulla++;
+            }
+            Console.WriteLine($"Az összes pozítiv szám: {osszesPozitiv}");
+            Console.WriteLine($"Az összes negatív szám: {osszesNegativ}");
+            Console.WriteLine($"Az összes nulla: {osszesNulla}");
+        }
+    }
+    static void vegrehajtArgFeladatok1(string[] args)
+    {
+        if (args.Length < 2)
         {
             Console.WriteLine("Nincs elég bemenet megadva");
         }
@@ -15,16 +43,16 @@ public class ObjektumGyak{
             int min = int.MaxValue, pozitivMin = int.MaxValue, negativMax = int.MinValue;
             int max = int.MinValue;
             int szam, ossz = 0;
-            for(int i = 0; i< args.Length; i++)
+            for (int i = 0; i < args.Length; i++)
             {
                 szam = int.Parse(args[i]);
-                if(min > szam)
+                if (min > szam)
                     min = szam;
                 if (pozitivMin > szam && szam >= 0)
                     pozitivMin = szam;
                 if (max < szam)
                     max = szam;
-                if(negativMax < szam && szam < 0)
+                if (negativMax < szam && szam < 0)
                     negativMax = szam;
 
                 ossz += szam;
@@ -36,8 +64,10 @@ public class ObjektumGyak{
             Console.WriteLine($"A legkisebb pozitiv szám: {pozitivMin}");
             Console.WriteLine($"A legnagyobb negatív szám: {negativMax}");
         }
+    }
 
-        /*
+    static void vegrehajtMacska()
+    {
         Macska m1 = new Macska("Cirmi", 2.3, false);
         Macska m2 = new Macska("Kormos", 1.4);
         m1.Eszik(0.2);
@@ -49,7 +79,10 @@ public class ObjektumGyak{
         m2.Eszik(0.3);
         m2.Futkos();
         Console.WriteLine(m2);
-        
+    }
+
+    static void vegrehajtSzamitogep()
+    {
         Szamitogep szg1 = new Szamitogep();
         Szamitogep szg2 = new Szamitogep(2000, false);
         szg1.Kapcsol();
@@ -58,8 +91,5 @@ public class ObjektumGyak{
         Console.WriteLine($"A számítógépre a másolás sikeres: {(szg2.Masol(1) ? "igen" : "nem")}");
         Console.WriteLine(szg1);
         Console.WriteLine(szg2);
-
-        */
     }
-
 }
