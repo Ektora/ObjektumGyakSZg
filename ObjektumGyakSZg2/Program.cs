5b2f9a5faa9b5964973ed653825dc724cd5549e7
Lucski Zsolt <69504623+Ektora@users.noreply.github.com>
2022-11-09 13:12:07 +0100

    Metódusos feladatok készen - asztali gépen debugolás

--- ObjektumGyakSZg2/Program.cs
+++ ObjektumGyakSZg2/Program.cs
@@ -46,7 +46,41 @@ namespace ObjektumGyakSZg2
 
             Console.WriteLine();
             Console.WriteLine($"A tömbben található elemek összege {Convert.ToInt32(args[0])} lépésközönként: {ElemekOsszegeLepeskozzel(Convert.ToInt32(args[0]), tomb)}");
+            Console.WriteLine($"A tömbben található páros elemek összege {Convert.ToInt32(args[0])} lépésközönként: {ParosElemekOsszegeLepeskozzel(Convert.ToInt32(args[0]), tomb)}");
+            Console.WriteLine($"A tömbben található páros elemek összege, {Convert.ToInt32(args[0])} lépésközönként, balról: {int balrol = ParosElemekOsszegeLepeskozzel(Convert.ToInt32(args[0]), tomb, false)}, jobbról: {int jobbrol = ParosElemekOsszegeLepeskozzel(Convert.ToInt32(args[0]), tomb, true)}".);
+            Console.WriteLine($"A páros elemek értéke {(balrol > jobbrol ? "balról jobbra" : "jobbról balra")} a nagyobbak!");
             }
+
+            static void ArgumentumFeldolgozas2(string[] args)
+        {
+            if (args.Length >=3 )
+            {
+                Console.WriteLine("Nem megfelelő számú argumentum");
+                return;
+            }
+            int[] tomb = args.Skip(2).Select(elem => Convert.ToInt32(elem)).ToArray();
+            foreach (int item in tomb)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Az {Convert.ToInt32(args[0])} indexű elem balról {ElemErtekeIndexEsIranyAlapjan(Convert.ToInt32(args[0]), tomb, false)}, jobbról : {ElemErtekeIndexEsIranyAlapjan(Convert.ToInt32(args[1]), tomb, true)}");
+            }
+        
+        public static int ElemErtekeIndexEsIranyAlapjan(int index, int[] tomb, bool jobbrol){
+            if(jobbrol == true){
+                return tomb.Reverse().Where((_,i) => i = index)[0];
+            }
+            return tomb.Where((_,i) => i = index)[0];
+        }
+
+         public static int ParosElemekOsszegeLepeskozzel(int[] tomb, int lepeskoz, bool jobbrol){
+            if(jobbrol)
+                return tomb.Reverse().Where((ertek, index) => (index + lepeskoz) % lepeskoz ==0 && ertek % 2 == 0).Sum();
+            return tomb.Where((ertek, index) => (index + lepeskoz) % lepeskoz ==0 && ertek % 2 == 0).Sum();
+         }
+
 
         public static int ParosElemekOsszegeLepeskozzel(int[] tomb, int lepeskoz){
             osszeg1 = tomb.Where((ertek, index) => (index + lepeskoz) % lepeskoz ==0 && ertek % 2 == 0).Sum();
